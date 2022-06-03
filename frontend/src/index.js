9fdcec4fc209a449190e2d2a389595c29df0b039
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-06-03 10:52:25 +0530

    react-time-ago added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from "react-cookie";
+
+import TimeAgo from 'javascript-time-ago'
+
+import en from 'javascript-time-ago/locale/en.json'
+import ru from 'javascript-time-ago/locale/ru.json'
+
+TimeAgo.addDefaultLocale(en)
+TimeAgo.addLocale(ru)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CookiesProvider>
